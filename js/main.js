fe258ec45f34b3bf2d03a89a49911582d9b79611
muzili <muzili@nb-chat.com>
2020-04-28 12:53:37 +0800

    Site updated: 2020-04-28 12:53:37

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 $(function () {
+
+  var bp = document.createElement('script');
+  var curProtocol = window.location.protocol.split(':')[0];
+  if (curProtocol === 'https') {
+      bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+  }
+  else {
+      bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+  }
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(bp, s);
+
   const isSnackbar = GLOBAL_CONFIG.Snackbar !== undefined
   const isTocContent = $('#sidebar .sidebar-toc__content').children().length > 0
   /**
