2a91368faecf0e9130e69b5d839e8aa07a7ee840
SpectreAlan <htmler.cn@gmail.com>
2020-11-26 14:12:07 +0800

    删除默认title

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-import React from 'react';
+import React from 'react'
 
 export default class MyDocument extends Document {
   static getInitialProps ({ renderPage }) {
