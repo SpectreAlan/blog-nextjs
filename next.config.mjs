178e3165b576868f38dbb2a188a60e4b3556ec9f
SpectreAlan <comjszoo@gmail.com>
2024-01-29 20:29:51 +0800

    add proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/blog/:path*',
+                destination: 'https://server.efv.cc/blog/:path*',
+            },
+            {
+                source: '/image-proxy/:path*',
+                destination: 'https://jszoo-file.oss-cn-beijing.aliyuncs.com/:path*',
+            },
+        ];
+    },
+};
 
 export default nextConfig;
