ed619c85e4d3b8902503a8eda1f440dad0f79a08
SpectreAlan <comjszoo@gmail.com>
2024-01-29 21:19:54 +0800

    add some css

--- src/app/home/FullPage.tsx
+++ src/app/home/FullPage.tsx
@@ -1,8 +1,14 @@
 import React, {useState, useEffect, useRef} from 'react'
 import {DoubleLeftOutlined} from '@ant-design/icons'
+import useFetch from "@/hooks/useFetch";
 
 const FullPage:React.FC = ()=>{
     const [count, setCount] = useState(0)
+    const {data} = useFetch({
+        url: '/blog/poem',
+        method: 'GET'
+    })
+    
     const InterRef = useRef()
     const changeCount = () => {
         const i = count < poem.length ? count + 1 : 0
@@ -20,10 +26,10 @@ const FullPage:React.FC = ()=>{
             clearInterval(timer)
         }
     }, [])
-    return <div bg={fullPage}>
+    return <div className={`bg-cover bg-center bg-fixed h-screen flex items-center relative" style="background-image: url(/image-proxy/blog/cover/${new Date().getDate()}.jpg);`}>
         <div className="txt">
             {poem.slice(0, count)}
-            <span className={count % 2 === 0 ? 'show' : 'hide'} ref={InterRef}> |</span>
+            <span className={count % 2 === 0 ? 'show' : 'hide'}> |</span>
         </div>
         <a href="#article-list" ><DoubleLeftOutlined/></a>
     </div>
