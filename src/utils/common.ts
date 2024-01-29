1cb547bd59472dbf30d50751750c1c9fda39cb16
SpectreAlan <comjszoo@gmail.com>
2024-01-29 20:31:31 +0800

    add utils of decrypt

---
+++ src/utils/common.ts
@@ -0,0 +1,10 @@
+import CryptoJS from "crypto-js";
+
+export function decrypt(data: string) {
+    const key: string = process.env.NEXT_PUBLIC_CRYPTO_SECRET_KEY!
+    const bytes = CryptoJS.AES.decrypt(data as string, key)
+    const decrypt = bytes.toString(CryptoJS.enc.Utf8)
+    return JSON.parse(decrypt)
+}
+
+
