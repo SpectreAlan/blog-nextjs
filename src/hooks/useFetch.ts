b41b211d6bd4bd2d72d8f9937565d0d774d46c0d
SpectreAlan <comjszoo@gmail.com>
2024-01-30 14:46:22 +0800

    Extract public functions

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,54 +1,22 @@
-import {useState, useEffect, useCallback} from 'react';
+import {useState,useCallback} from 'react';
 import {message} from 'antd'
-import {decrypt} from "@/utils/common";
+import httpRequest from "@/utils/fetch";
 
-interface UseFetchProps {
-    url: string;
-    method: 'GET' | 'POST'
-    data?: any;
-}
 
-interface UseFetchResult<T> {
-    response: T | null;
-    loading: boolean;
-    handleFetch: () => void
-}
-
-const useFetch = <T>({url, method, data}: UseFetchProps): UseFetchResult<T> => {
+const useFetch = <T>(props: Common.UseFetchProps): Common.UseFetchResult<T> => {
     const [response, setData] = useState<T | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
 
     const handleFetch = useCallback(async () => {
         setLoading(true)
-        try {
-            const fetchConfig: RequestInit = {
-                method
-            }
-            if (method === 'GET') {
-                const queryString = new URLSearchParams(data).toString();
-                url += `?${queryString}`
-            } else {
-                fetchConfig.headers = {'Content-Type': 'application/json'}
-                fetchConfig.body = JSON.stringify(data)
-            }
-            const response = await fetch(url, fetchConfig);
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const result: string = await response.text();
-            const {data: res, message: msg, code} = decrypt(result)
-            if (code) {
-                message.error(msg)
-            } else {
-                setData(res);
-            }
-        } catch (error) {
-            console.log(error);
-            // message.error(error as string)
-        } finally {
-            setLoading(false);
+        const res:Common.FetchResult<T> = await httpRequest(props)
+        const {data, message: msg, code} = res
+        if (code) {
+            message.error(msg)
+        } else {
+            setData(data);
         }
+        setLoading(false)
     }, []);
 
     return {response, loading, handleFetch};
