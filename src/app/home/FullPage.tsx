57b19fec223dc1accc575dd32689c96e1ea0af62
SpectreAlan <comjszoo@gmail.com>
2024-01-30 14:46:43 +0800

    finish fullPage

--- src/app/home/FullPage.tsx
+++ src/app/home/FullPage.tsx
@@ -1,39 +1,62 @@
 import React, {useState, useEffect} from 'react'
 import {DoubleLeftOutlined} from '@ant-design/icons'
-import useFetch from "@/hooks/useFetch";
+import httpRequest from "@/utils/fetch";
+
+interface IPoem {
+    content: string
+}
 
 let index = 0
-let count = 0
+let timer: any = null
 const FullPage: React.FC = () => {
-    // const [count, setCount] = useState(0)
-    // const {response} = useFetch<{ content: string }[]>({
-    //     url: '/blog/poem',
-    //     method: 'GET'
-    // })
+    const [poem, setPoem] = useState<IPoem[]>([{content: '茶若醉人何须酒'}])
+    const [count, setCount] = useState(0)
 
-    // useEffect(() => {
-    //     const timer = setInterval(() => {
-    //         if (response?.length) {
-    //             const i = count < response[index].content.length ? count + 1 : 0
-    //             if (i === 0) {
-    //                 index = Math.floor(Math.random() * (response.length))
-    //             }
-    //             setCount(i)
-    //         }
-    //
-    //     }, 300)
-    //     return () => {
-    //         clearInterval(timer)
-    //     }
-    // }, [])
+    const query = async () => {
+        const res: Common.FetchResult<{ list: IPoem[] }> = await httpRequest({url: '/blog/poem'})
+        const {data = {list: []}} = res
+        setPoem(data.list)
+    }
+
+    useEffect(() => {
+        query()
+    }, [])
+    useEffect(() => {
+        if (poem.length) {
+            clearInterval(timer)
+            timer = setInterval(() => {
+                setCount(prevCount => {
+                    const i = prevCount < poem[index].content.length ? prevCount + 1 : 0;
+                    if (i === 0) {
+                        index = Math.floor(Math.random() * (poem.length))
+                    }
+                    return i;
+                });
+            }, 300)
+        }
+        return () => {
+            clearInterval(timer)
+        }
+    }, [poem])
     return <div
-        className={`bg-cover bg-center bg-fixed h-screen flex items-center relative" style="background-image: url(/image-proxy/blog/cover/${new Date().getDate()}.jpg);`}>
-        <div className="txt">
-            {/*{response?.[index]?.content.slice(0, count)}*/}
-            <span className={count % 2 === 0 ? 'show' : 'hide'}> |</span>
+        className={`bg-cover bg-center bg-fixed h-[100vh] relative`}
+        style={{backgroundImage: `url(/image-proxy/blog/cover/${new Date().getDate()}.jpg)`}}>
+        <div
+            className="absolute w-full top-1/2 text-center transform-translate-y-1/2 text-white sm:text-2xl md:text-4xl">
+            {poem?.[index]?.content.slice(0, count)}
+            <span
+                className={`text-gray-200 ml-2 ${count % 2 === 0 ? 'show' : 'hide'}`}
+            >
+                |
+            </span>
         </div>
-        <a href="#article-list"><DoubleLeftOutlined rev=''/></a>
+        <a href="#article-list"
+           className='transform rotate-[-90deg] left-1/2 transform-translate-x-1/2 text-3xl bottom-0 absolute  cursor-pointer'
+           style={{animation: 'animation 1s linear infinite'}}>
+            <DoubleLeftOutlined rev=''/>
+        </a>
     </div>
 }
+
 
 export default FullPage
