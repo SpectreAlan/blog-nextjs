72856a908056b623a415bfd17ec02d27d4c1c0e4
SpectreAlan <comjszoo@gmail.com>
2024-01-31 20:55:19 +0800

    add notice component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 import FullPage from "@/app/home/FullPage";
 import Articles from "@/app/home/Articles";
+import Aside from "@/app/home/Aside";
 import React from "react";
+import {Col, Row} from 'antd'
 
 const HomePage: React.FC<Common.IProps> = ({searchParams}) => {
     return (
         <div className="min-h-screen">
             <FullPage/>
-            <Articles searchParams={searchParams}/>
+            <Row className='items-start mx-auto px-15 max-w-[1200px] flex py-4'>
+                <Col lg={17} md={17} sm={24} xs={24}>
+                    <Articles searchParams={searchParams}/>
+                </Col>
+                <Col lg={6} md={6} sm={0} xs={0} offset={1}>
+                    <Aside/>
+                </Col>
+            </Row>
+
         </div>
     );
 }
