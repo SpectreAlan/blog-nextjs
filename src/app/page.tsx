637dcf8836c47087e2aa1fa674d070346a0d69ee
SpectreAlan <comjszoo@gmail.com>
2024-01-31 13:16:41 +0800

    Add address bar parameters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-'use client'
 import FullPage from "@/app/home/FullPage";
+import Articles from "@/app/home/Articles";
+import React from "react";
 
-const HomePage = ()=> {
+const HomePage: React.FC<Common.IProps> = ({searchParams}) => {
     return (
         <div className="min-h-screen">
             <FullPage/>
+            <Articles searchParams={searchParams}/>
         </div>
     );
 }
