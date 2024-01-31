730a7d4cee01ceac266391f23429bdbf3f5b8686
SpectreAlan <comjszoo@gmail.com>
2024-01-31 13:15:33 +0800

    add article types

--- src/types/article.typings.d.ts
+++ src/types/article.typings.d.ts
@@ -2,6 +2,9 @@ declare namespace Article {
     interface ArticleItem {
         id: string;
         title: string;
+        cover: string;
+        description: string;
+        createdAt: string;
         category: {
             title: string
         }
