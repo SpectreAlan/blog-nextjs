ad22dcd85364bc453f2bfbcb25bea2f29867d8c9
SpectreAlan <comjszoo@gmail.com>
2024-02-01 23:51:28 +0700

    add article nav

---
+++ src/app/detail/[id]/Nav.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from 'react'
+import MarkdownNavbar from 'markdown-navbar'
+import 'markdown-navbar/dist/navbar.css';
+const Nav:React.FC<{content: string}> = ({content})=>{
+    return <div className="w-80 fixed h-full top-0 left-0 pt-10 fuck-shadow">
+        <h3 className='text-center'>文章目录</h3>
+        <MarkdownNavbar
+            className="article-menu"
+            source={content}
+            headingTopOffset={50}
+        />
+    </div>
+}
+
+export default Nav
