afbbf0876e65b6294abbc83fe2317af65d69b540
SpectreAlan <comjszoo@gmail.com>
2024-02-01 23:51:52 +0700

    add copy component

---
+++ src/app/detail/[id]/Copy.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React from "react";
+import {CopyOutlined} from "@ant-design/icons";
+import {message} from 'antd'
+
+const Copy: React.FC<{ code: string }> = ({code}) => {
+
+    const copy = () => {
+        navigator.clipboard.writeText(code)
+        message.success('已复制到剪切板')
+    }
+
+    return <CopyOutlined
+        rev=''
+        className='absolute right-2 top-2 cursor-pointer hover:scale-125'
+        onClick={() => copy()}
+        title='点击复制'
+    />
+}
+
+export default Copy
