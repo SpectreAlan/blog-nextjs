c27b0c266d753513840f96ef7eb20ccdfe20d1e7
SpectreAlan <comjszoo@gmail.com>
2024-02-02 20:42:39 +0800

    add comment types

---
+++ src/types/comment.typings.d.ts
@@ -0,0 +1,19 @@
+declare namespace Comment {
+    interface Item {
+        id: string;
+        content: string;
+        article: string;
+        nickName: string;
+        email: string;
+        parentId: string;
+        parentName: string;
+        author: number;
+        pinned: number;
+        createdAt: string
+    }
+    interface Info {
+        article: string
+        parentId: string
+        nickName?: string
+    }
+}
