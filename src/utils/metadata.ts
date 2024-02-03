af600e118b12199732adf6950c6c31d65b52a821
SpectreAlan <comjszoo@gmail.com>
2024-02-03 19:56:43 +0800

    add default metatata

---
+++ src/utils/metadata.ts
@@ -0,0 +1,11 @@
+const DefaultMetadata = {
+    title: `SpectreAlan's Blog`,
+    keywords: 'SpectreAlan,web前端,Nginx,Linux,Vue,Flutter,React,NextJS,react-hooks,NodeJS,NestJS, Vercel',
+    description: "SpectreAlan 的个人博客，一个有内涵的web前端，专注vue/react/nodejs/flutter",
+    authors: [{name: 'SpectreAlan', url: 'https://efv.cc'}],
+    creator: 'SpectreAlan',
+    icons: {
+        shortcut: '/favicon.ico',
+    }
+}
+export default DefaultMetadata
