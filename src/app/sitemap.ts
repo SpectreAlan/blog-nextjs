80077f970a2d8243edbda285ecad42e2de441ff1
SpectreAlan <comjszoo@gmail.com>
2024-02-03 19:56:11 +0800

    add sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,24 @@
 import {MetadataRoute} from "next";
+import httpRequest from "@/utils/fetch";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    return [{id: '11111', date: '2023-05-05'},{id: '11111', date: '2023-05-05'}].map((product) => ({
-        url: `https://xx.com/product/${product.id}`,
-        lastModified: product.date,
-    }))
+    const baseURL = process.env.NEXT_PUBLIC_HOST
+    const now = new Date()
+    const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDay()}`
+    const sitemap: MetadataRoute.Sitemap = [
+        {url: baseURL + '/about', lastModified: today, changeFrequency: 'daily'},
+        {url: baseURL + '/timeLine', lastModified: today, changeFrequency: 'daily'},
+        {url: baseURL + '/', lastModified: today, changeFrequency: 'daily'},
+    ]
+    const res: { list: Article.ArticleItem[] } | null = await httpRequest({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/timeLine`,
+    })
+    res?.list?.map((article)=>{
+        sitemap.push({
+            url: baseURL + '/detail/' + article.id,
+            lastModified: article.updatedAt,
+            changeFrequency: 'weekly'
+        })
+    })
+    return sitemap
 }
