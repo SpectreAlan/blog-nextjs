070cf470f69147bfc43fa00b8d69f2bb97e58e42
SpectreAlan <comjszoo@gmail.com>
2024-02-03 19:58:33 +0800

    add tag params

--- src/app/home/Tags.tsx
+++ src/app/home/Tags.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import {TagOutlined} from "@ant-design/icons";
+import Link from 'next/link';
 
 const RecentUpdate: React.FC<{ tags: string[] }> = ({tags}) => {
     return <div className="p-4 rounded-lg mb-4 overflow-hidden text-center fuck-shadow">
         <div className='text-left mb-2 font-bold'><TagOutlined className='mr-2' rev=''/>标签</div>
         <div className='flex flex-wrap justify-around'>
             {
-                tags.map((item, index) => (
+                tags.map((tag, index) => (
+                    <Link
+                        key={tag}
+                        href={`/?tags=${tag}`}
+                        as={`/?tags=${tag}`}
+                    >
                     <span
                         className='p-1'
-                        key={index}
                         style={{
                             color: `rgb(${Math.floor(Math.random() * 201)}, ${Math.floor(Math.random() * 201)}, ${Math.floor(Math.random() * 201)} )`,
                             fontSize: Math.floor(Math.random() * 15 + 15) + 'px'
                         }}
-                    >{item}
+                    >{tag}
                 </span>
+                    </Link>
                 ))
             }
         </div>
