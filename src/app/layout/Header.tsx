e45fe32e42a1c482376ff410e61e099061cdc8e2
SpectreAlan <comjszoo@gmail.com>
2024-02-03 19:58:07 +0800

    fixed bug

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, {useState, useEffect} from 'react'
-import { Avatar} from 'antd'
+import {Avatar} from 'antd'
 import _ from 'lodash';
 import Nav from "@/app/layout/Nav";
 
@@ -16,15 +16,14 @@ const Header = () => {
         }, 300)
     })
     return (
-        <div className={`box-border py-0 px-4 fixed z-50 w-full top-0 left-0 bg-opacity-60 bg-white shadow-md transition-transform duration-800 ${isUp ? 'opacity-0 transform translate-y-[-30px]' : 'opacity-100 transform translate-x-0 translate-y-0 translate-z-0'}`}>
+        <div
+            className={`box-border py-0 px-4 fixed z-50 w-full top-0 left-0 bg-opacity-60 bg-white shadow-md transition-transform duration-800 ${isUp ? 'opacity-0 transform translate-y-[-30px]' : 'opacity-100 transform translate-x-0 translate-y-0 translate-z-0'}`}>
             <div className="flex justify-between items-center">
-                <div className="log">
+                <div>
                     <Avatar className='mr-2' size="large" src='/image-proxy/blog/common/logo.png'/>
                     <span>{'AlanGrady\'s blogs'}</span>
                 </div>
-                <div className="nav">
-                    <Nav/>
-                </div>
+                <Nav/>
             </div>
         </div>
     )
