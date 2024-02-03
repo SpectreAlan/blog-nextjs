2ea42ac1f0b75230d04e24666ff499fc614a0f0d
SpectreAlan <comjszoo@gmail.com>
2024-02-03 19:57:50 +0800

    edit category params

--- src/app/home/Category.tsx
+++ src/app/home/Category.tsx
@@ -7,13 +7,13 @@ const RecentUpdate: React.FC<{ categoryList: Aside.Category[] }> = ({categoryLis
     return <div className="p-4 rounded-lg mb-4 overflow-hidden text-center fuck-shadow">
         <div className='text-left mb-2 font-bold'><FolderOpenOutlined className='mr-2' rev=''/>分类</div>
         {
-            categoryList.map((item, index) => (
+            categoryList.map((item) => (
                 <Link
                     href={`/?category=${item.category}`}
                     as={`/?category=${item.category}`}
-                    key={index}
+                    key={item.category}
                 >
-                    <Row className='py-1 text-left hover:bg-blue-500 hover:text-white hover:px-2 hover:rounded' key={index} gutter={8}>
+                    <Row className='py-1 text-left hover:bg-blue-500 hover:text-white hover:px-2 hover:rounded' gutter={8}>
                         <Col span={20}>
                             {item.category}
                         </Col>
