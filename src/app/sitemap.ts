7ab409f8bcecdfdeb3f530bd498f06102939a624
SpectreAlan <comjszoo@gmail.com>
2024-02-04 22:18:20 +0700

    fixed bug

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,9 @@ import httpRequest from "@/utils/fetch";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseURL = process.env.NEXT_PUBLIC_HOST
     const now = new Date()
-    const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
+    const month: number = now.getMonth() + 1
+    const day: number = now.getDate()
+    const today = `${now.getFullYear()}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`
     const sitemap: MetadataRoute.Sitemap = [
         {url: baseURL + '/about', lastModified: today, changeFrequency: 'daily'},
         {url: baseURL + '/timeLine', lastModified: today, changeFrequency: 'daily'},
@@ -17,7 +19,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     res?.list?.map((article)=>{
         sitemap.push({
             url: baseURL + '/detail/' + article.id,
-            lastModified: article.updatedAt.split(' ')[0],
+            lastModified: article.updatedAt,
             changeFrequency: 'weekly'
         })
     })
