02ca1cea3e9e3227a7e1f75ae786a506afed68eb
SpectreAlan <comjszoo@gmail.com>
2024-02-04 14:35:44 +0700

    add Icon's property of rev

--- src/app/detail/[id]/CommentForm.tsx
+++ src/app/detail/[id]/CommentForm.tsx
@@ -1,8 +1,7 @@
-import {Button, Form, Input, message} from 'antd';
+import {Button, Form, Input} from 'antd';
 import {MailOutlined, MessageOutlined, UserOutlined} from '@ant-design/icons';
 import React, {useState} from 'react';
 import httpRequest from "@/utils/fetch";
-import platform from 'platform'
 
 interface IProps {
     info: Comment.Info
@@ -12,7 +11,7 @@ interface IProps {
 const CommentForm: React.FC<IProps> = ({info, handelFinished}) => {
     const [loading, setLoading] = useState(false)
 
-    const [form] =Form.useForm()
+    const [form] = Form.useForm()
 
     const placeholder = info?.nickName ? '@' + info?.nickName : 'Talk is cheap,show me the code!'
     const onFinish = async (values: { [key: string]: string }) => {
@@ -42,13 +41,13 @@ const CommentForm: React.FC<IProps> = ({info, handelFinished}) => {
     };
     return (<Form onFinish={onFinish} form={form}>
         <Form.Item
-            label={<span><UserOutlined/>&nbsp;昵称&nbsp;</span>} name="nickName"
+            label={<span><UserOutlined rev=''/>&nbsp;昵称&nbsp;</span>} name="nickName"
             rules={[{required: true, message: '请输入您的昵称!',}]}
         >
             <Input placeholder="请输入您的昵称..."/>
         </Form.Item>
         <Form.Item
-            label={<span><MailOutlined/>&nbsp;邮箱&nbsp;</span>} name="email"
+            label={<span><MailOutlined rev=''/>&nbsp;邮箱&nbsp;</span>} name="email"
             rules={[{type: 'email', message: '邮箱格式有误！',}, {
                 required: true,
                 message: '必填!但不会公开，站长回复以后会收到邮件通知',
@@ -57,7 +56,7 @@ const CommentForm: React.FC<IProps> = ({info, handelFinished}) => {
             <Input placeholder="请输入您的邮箱,站长回复以后会收到邮件通知"/>
         </Form.Item>
         <Form.Item
-            label={<span><MessageOutlined/>&nbsp;评论&nbsp;</span>} name="content"
+            label={<span><MessageOutlined rev=''/>&nbsp;评论&nbsp;</span>} name="content"
             rules={[{required: true, message: '请输入您的评论!',}]}
         >
             <Input.TextArea placeholder={placeholder}/>
