6647b25d8ccf812803c29a0819ed603b1210ecfa
SpectreAlan <comjszoo@gmail.com>
2024-02-04 13:58:48 +0700

    fixed some bug

--- src/app/home/Profile.tsx
+++ src/app/home/Profile.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Profile: React.FC<{ aside: Aside.Items }> = ({aside}) => {
     return <div className="p-4 rounded-lg mb-4 overflow-hidden text-center fuck-shadow mt-4">
-        <Image alt='logo'  width={44} height={44} src='/image-proxy/blog/common/logo.png'/>
+        <Image alt='logo' width={44} height={44} src='/image-proxy/blog/common/logo.png'/>
         <h2>AlanGrady</h2>
         <p>如果第一次失败了，那这是1.0版本，请继续努力</p>
         <Row>
@@ -20,11 +20,15 @@ const Profile: React.FC<{ aside: Aside.Items }> = ({aside}) => {
             <BookOutlined className='mr-3' rev=''/>加入书签
         </div>
         <div className="flex justify-around text-2xl my-4">
-            <Tooltip title='SpectreAlan'><a href="https://github.com/SpectreAlan" target="_blank"><GithubOutlined/></a></Tooltip>
-            <Tooltip title='comjszoo@gmail.com@gmail.com'><a href="mailto:comjszoo@gmail.com"
-                                                             target="_blank"><MailOutlined/></a></Tooltip>
-            <Tooltip title='SpectreAlan'><a href="https://twitter.com/SpectreAlan"
-                                            target="_blank"><TwitterOutlined/></a></Tooltip>
+            <Tooltip title='SpectreAlan'>
+                <a href="https://github.com/SpectreAlan" target="_blank"><GithubOutlined rev/></a>
+            </Tooltip>
+            <Tooltip title='comjszoo@gmail.com@gmail.com'>
+                <a href="mailto:comjszoo@gmail.com" target="_blank"><MailOutlined rev/></a>
+            </Tooltip>
+            <Tooltip title='SpectreAlan'>
+                <a href="https://twitter.com/SpectreAlan" target="_blank"><TwitterOutlined rev/></a>
+            </Tooltip>
         </div>
     </div>
 }
