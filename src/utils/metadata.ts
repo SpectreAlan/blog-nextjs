f51dca8a19c80bd48f6a14b1e7a850a66dcb9a45
SpectreAlan <comjszoo@gmail.com>
2024-02-06 23:59:40 +0700

    Add types

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,4 +1,6 @@
-const DefaultMetadata = {
+import type {Metadata} from "next";
+
+const DefaultMetadata: Metadata = {
     title: `SpectreAlan's Blog`,
     keywords: 'SpectreAlan,web前端,Nginx,Linux,Vue,Flutter,React,NextJS,react-hooks,NodeJS,NestJS, Vercel',
     description: "SpectreAlan 的个人博客，一个有内涵的web前端，专注vue/react/nodejs/flutter",
@@ -6,6 +8,6 @@ const DefaultMetadata = {
     creator: 'SpectreAlan',
     icons: {
         shortcut: '/favicon.ico',
-    }
+    },
 }
 export default DefaultMetadata
