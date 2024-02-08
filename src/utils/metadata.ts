6a136cb3f550ca99e94bb9c8852805e6c32f369e
SpectreAlan <comjszoo@gmail.com>
2024-02-08 14:04:14 +0800

    edit default meta data

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -4,7 +4,7 @@ const DefaultMetadata: Metadata = {
     title: `SpectreAlan's Blog`,
     keywords: 'SpectreAlan,web前端,Nginx,Linux,Vue,Flutter,React,NextJS,react-hooks,NodeJS,NestJS, Vercel',
     description: "SpectreAlan 的个人博客，一个有内涵的web前端，专注vue/react/nodejs/flutter",
-    authors: [{name: 'SpectreAlan', url: 'https://efv.cc'}],
+    authors: [{name: 'SpectreAlan', url: 'https://jszoo.com'}],
     creator: 'SpectreAlan',
     icons: {
         shortcut: '/favicon.ico',
