2efe095745f9c4c29bcbe196a8289453016fd2e3
SpectreAlan <comjszoo@gmail.com>
2024-07-27 14:00:23 +0800

    fix

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -3,7 +3,7 @@ import type {Metadata} from "next";
 const DefaultMetadata: Metadata = {
     title: `SpectreAlan's Blog`,
     keywords: 'SpectreAlan,web前端,Nginx,Linux,Vue,Flutter,React,NextJS,react-hooks,NodeJS,NestJS, Vercel',
-    description: "SpectreAlan 的个人博客，一个有内涵的web前端，专注vue/react/nodejs/flutter",
+    description: "SpectreAlan的个人博客，一个有内涵的web前端，专注vue/react/nodejs/flutter",
     authors: [{name: 'SpectreAlan', url: 'https://jszoo.com'}],
     creator: 'SpectreAlan',
     icons: {
